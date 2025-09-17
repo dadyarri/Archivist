6df9fcb0cb5e42ad3a4cbe24668675fd1446509f
Daniil <dadyarri@gmail.com>
2025-09-17 21:31:49 +0300

    migrate settings page to viewmodel

---
+++ Archivist/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,166 @@
+﻿using Archivist.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
+
+namespace Archivist.ViewModels
+{
+    public partial class SettingsPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string vaultPath = "Папка не выбрана";
+        [ObservableProperty]
+        private string subDirectoryPath = "Папка не выбрана";
+        [ObservableProperty]
+        private string filenameFormat = "Сессия №{number}. {name}";
+        [ObservableProperty]
+        private Visibility filenameFormatErrorVisibility = Visibility.Collapsed;
+        [ObservableProperty]
+        private Visibility saveNotificationVisibility = Visibility.Collapsed;
+
+        private AppConfig _config;
+        private readonly char[] _bannedChars = new[] { '*', '"', '\\', '/', ':', '?', '<', '>', '|' };
+        private Timer _saveTimer;
+        private DispatcherQueue _dispatcherQueue;
+        private Action _showSaveNotificationAction;
+
+        public SettingsPageViewModel(DispatcherQueue dispatcherQueue, Action showSaveNotificationAction)
+        {
+            _dispatcherQueue = dispatcherQueue;
+            _showSaveNotificationAction = showSaveNotificationAction;
+            _config = AppConfig.Load();
+
+            if (_config.Vault != string.Empty)
+            {
+                vaultPath = _config.Vault;
+            }
+
+            if (_config.SubDirectory != string.Empty)
+            {
+                subDirectoryPath = _config.SubDirectory;
+            }
+
+            if (_config.Format != string.Empty)
+            {
+                filenameFormat = _config.Format;
+            }
+
+            _saveTimer = new Timer(800);
+            _saveTimer.Elapsed += SaveTimer_Elapsed;
+            _saveTimer.AutoReset = false;
+
+        }
+
+        partial void OnFilenameFormatChanged(string value)
+        {
+            if (value.IndexOfAny(_bannedChars) >= 0)
+            {
+                FilenameFormatErrorVisibility = Visibility.Visible;
+            }
+            else
+            {
+                FilenameFormatErrorVisibility = Visibility.Collapsed;
+
+                _saveTimer.Stop();
+                _saveTimer.Start();
+            }
+        }
+
+        [RelayCommand]
+        private async Task SelectFolderAsync()
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.FileTypeFilter.Add("*");
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.ViewMode = PickerViewMode.List;
+
+            var hwnd = GetWindowHandle();
+            if (hwnd == IntPtr.Zero)
+            {
+                VaultPath = "Error: Main window not found.";
+                return;
+            }
+
+            InitializeWithWindow.Initialize(folderPicker, hwnd);
+
+            var folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null && Directory.EnumerateDirectories(folder.Path, ".obsidian").Any())
+            {
+                VaultPath = folder.Path;
+                _config.Vault = folder.Path;
+                await _config.SaveAsync();
+            }
+            else
+            {
+                await ShowErrorDialogAsync("Ошибка", "Выберите директорию, являющуюся хранилищем Obsidian");
+            }
+        }
+
+        [RelayCommand]
+        private async Task SelectSubFolderAsync()
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.FileTypeFilter.Add("*");
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.ViewMode = PickerViewMode.List;
+
+            var hwnd = GetWindowHandle();
+            if (hwnd == IntPtr.Zero)
+            {
+                SubDirectoryPath = "Error: Main window not found.";
+                return;
+            }
+
+            InitializeWithWindow.Initialize(folderPicker, hwnd);
+
+            var folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null && folder.Path.StartsWith(VaultPath))
+            {
+                SubDirectoryPath = folder.Path;
+                _config.SubDirectory = folder.Path;
+                await _config.SaveAsync();
+            }
+            else
+            {
+                await ShowErrorDialogAsync("Ошибка", $"Выберите директорию внутри хранилища Obsidian {VaultPath}");
+            }
+        }
+
+        private async void SaveTimer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            _config.Format = FilenameFormat;
+            await _config.SaveAsync();
+            _dispatcherQueue.TryEnqueue(() => _showSaveNotificationAction?.Invoke());
+        }
+
+        private IntPtr GetWindowHandle()
+        {
+            Window? mainWindow = (Application.Current as App)?.MainWindow;
+            return mainWindow != null ? WindowNative.GetWindowHandle(mainWindow) : IntPtr.Zero;
+        }
+
+        private Task ShowErrorDialogAsync(string title, string content)
+        {
+            _dispatcherQueue.TryEnqueue(DispatcherQueuePriority.Normal, async () =>
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = title,
+                    Content = content,
+                    CloseButtonText = "OK"
+                };
+                await dialog.ShowAsync();
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
--- Archivist/Views/SettingsPage.xaml.cs
+++ Archivist/Views/SettingsPage.xaml.cs
@@ -1,14 +1,10 @@
-﻿using Archivist.Models;
+﻿using Archivist.ViewModels;
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
 using System;
-using System.IO;
-using System.Linq;
-using System.ServiceModel.Channels;
 using System.Threading.Tasks;
-using Windows.Storage.Pickers;
-using WinRT.Interop;
 
 namespace Archivist.Views
 {
@@ -17,139 +13,16 @@ namespace Archivist.Views
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
-        public AppConfig _config { get; set; }
-        private DispatcherTimer _saveTimer;
-        private readonly char[] _bannedChars = new[] { '*', '"', '\\', '/', ':', '?', '<', '>', '|' };
+        private SettingsPageViewModel ViewModel { get; }
 
         public SettingsPage()
         {
             InitializeComponent();
-            _config = AppConfig.Load();
-            if (_config.Vault != string.Empty)
-            {
-                SelectedFolderText.Text = _config.Vault;
-            }
-
-            if (_config.SubDirectory != string.Empty)
-            {
-                SelectedSubFolderText.Text = _config.SubDirectory;
-            }
-
-            if (_config.Format != string.Empty)
-            {
-                FilenameFormatInput.Text = _config.Format;
-            }
-
-            _saveTimer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(800)
-            };
-            _saveTimer.Tick += SaveTimer_Tick;
+            ViewModel = new SettingsPageViewModel(DispatcherQueue.GetForCurrentThread(), async () => { await ShowSaveNotificationAsync(); });
+            DataContext = ViewModel;
         }
 
-        private async void SelectFolderButton_Click(object sender, RoutedEventArgs e)
-        {
-            var folderPicker = new FolderPicker();
-            folderPicker.FileTypeFilter.Add("*");
-            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
-            folderPicker.ViewMode = PickerViewMode.List;
-
-            Window? mainWindow = (Application.Current as App)?.MainWindow;
-            if (mainWindow == null)
-            {
-                SelectedFolderText.Text = "Error: Main window not found.";
-                return;
-            }
-
-            var hwnd = WindowNative.GetWindowHandle(mainWindow);
-            InitializeWithWindow.Initialize(folderPicker, hwnd);
-
-            var folder = await folderPicker.PickSingleFolderAsync();
-            if (folder != null && Directory.EnumerateDirectories(folder.Path, ".obsidian").Any())
-            {
-                SelectedFolderText.Text = folder.Path;
-                _config.Vault = folder.Path;
-                await _config.SaveAsync();
-            }
-            else
-            {
-                var dialog = new ContentDialog
-                {
-                    Title = "Ошибка",
-                    Content = $"Выберите директорию, являющуюся хранилищем Obsidian",
-                    CloseButtonText = "OK",
-                    XamlRoot = this.XamlRoot
-                };
-                await dialog.ShowAsync();
-            }
-        }
-
-        private async void SelectSubFolderButton_Click(object sender, RoutedEventArgs e)
-        {
-            var folderPicker = new FolderPicker();
-            folderPicker.FileTypeFilter.Add("*");
-            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
-            folderPicker.ViewMode = PickerViewMode.List;
-
-            Window? mainWindow = (Application.Current as App)?.MainWindow;
-            if (mainWindow == null)
-            {
-                SelectedFolderText.Text = "Error: Main window not found.";
-                return;
-            }
-
-            var hwnd = WindowNative.GetWindowHandle(mainWindow);
-            InitializeWithWindow.Initialize(folderPicker, hwnd);
-
-            var folder = await folderPicker.PickSingleFolderAsync();
-            if (folder != null && folder.Path.StartsWith(SelectedFolderText.Text))
-            {
-                SelectedSubFolderText.Text = folder.Path;
-                _config.SubDirectory = folder.Path;
-                await _config.SaveAsync();
-            }
-            else
-            {
-                var dialog = new ContentDialog
-                {
-                    Title = "Ошибка",
-                    Content = $"Выберите директорию внутри хранилища Obsidian {SelectedFolderText.Text}",
-                    CloseButtonText = "OK",
-                    XamlRoot = this.XamlRoot
-                };
-                await dialog.ShowAsync();
-            }
-        }
-
-        private void FilenameFormatInput_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var text = FilenameFormatInput.Text;
-
-            if (text.IndexOfAny(_bannedChars) >= 0)
-            {
-                FilenameFormatError.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                FilenameFormatError.Visibility = Visibility.Collapsed;
-
-                _saveTimer.Stop();
-                _saveTimer.Start();
-            }
-        }
-
-        private async void SaveTimer_Tick(object? sender, object e)
-        {
-            _saveTimer.Stop();
-
-            // Save the current value to your config
-            _config.Format = FilenameFormatInput.Text;
-            await _config.SaveAsync();
-            ShowSaveNotification();
-        }
-
-
-        private async void ShowSaveNotification(int milliseconds = 1500)
+        private async Task ShowSaveNotificationAsync(int milliseconds = 1500)
         {
             SaveNotification.Opacity = 0;
             SaveNotification.Visibility = Visibility.Visible;
